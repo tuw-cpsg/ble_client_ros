bcc11854c8461b3e0adf0951ef9a7152c6d2fe18
Alexander Temper <Alexander.Temper@gmail.com>
2018-11-11 10:23:45 +0100

    Cpp Node for Spektrum Drone Controll

--- ble_client_node/src/node.cpp
+++ ble_client_node/src/node.cpp
@@ -1,13 +1,11 @@
 #include <ros/ros.h>
-#include <ble_client_msgs/HandleUInt8Array.h>
-#include <ble_client_msgs/HandleUInt16Array.h>
-
 #include <ble_client_cpp/ble_client.h>
 
 #include <stdlib.h>
 #include <glib.h>
 #include <sstream>
 #include <signal.h>
+#include <sensor_msgs/Joy.h>
 
 #define DEFAULT_ADAPTER "hci0"
 #define ADDR_TYPE "random"
@@ -15,32 +13,43 @@
 bool keepRunning = true;
 BleClient *ble_client;
 
-ros::Publisher handle_pub;
+typedef struct {
+    uint16_t thrust = 0;
+    uint16_t roll = 1024;
+    uint16_t pitch = 1024;
+    uint16_t yaw = 1024;
+    uint16_t arm = 0;
+    uint16_t acro = 0;
+} controlData;
 
-void gattNotificationListener_cb(const uint16_t handle, const int size, const uint8_t *value)
-{
-    ble_client_msgs::HandleUInt8Array msg;
-    msg.handle = handle;
-    msg.data   = std::vector<uint8_t>(value, value + size * sizeof(uint8_t));
 
-    handle_pub.publish(msg);
+controlData joydaten;
+//yaw ,thrust ,roll ,pitch ,arm ,acro
+
+
+void joyCallback(const sensor_msgs::Joy::ConstPtr& data){
+
+    joydaten.yaw     = uint16_t((((-1*data->axes[0])+1))*1023.5);
+    joydaten.thrust = 0;
+    if (data->axes[1] >= 0.0){
+        joydaten.thrust  = uint16_t((data->axes[1])*2047);    
+    }
+        
+    joydaten.roll    = uint16_t((((-1*data->axes[3])+1))*1023.5);  
+    joydaten.pitch   = uint16_t((data->axes[4]+1)*1023.5);
+
+    joydaten.arm = uint16_t(data->buttons[4]*2047);
+    joydaten.acro = uint16_t(data->buttons[5]*2047);
 }
 
-void gattWriteCmdUInt8Array_cb(const ble_client_msgs::HandleUInt8Array::ConstPtr& msg)
-{
+void sendData(const ros::TimerEvent&){
     if(ble_client != NULL && ble_client->isConnected() == true)
     {
-        ble_client->gattWriteCmd(msg->handle, (const uint8_t *)(msg->data.data()), msg->data.size() * sizeof(uint8_t));
+        //ROS_INFO("%d %d %d %d %d %d",steuerdaten[0],steuerdaten[1],steuerdaten[2],steuerdaten[3],steuerdaten[4],steuerdaten[5]);
+        ble_client->gattWriteCmd(0x0014, (const uint8_t *)(&joydaten),  sizeof(joydaten));
     }
 }
 
-void gattWriteCmdUInt16Array_cb(const ble_client_msgs::HandleUInt16Array::ConstPtr& msg)
-{
-    if(ble_client != NULL && ble_client->isConnected() == true)
-    {
-        ble_client->gattWriteCmd(msg->handle, (const uint8_t *)(msg->data.data()), msg->data.size() * sizeof(uint16_t));
-    }
-}
 
 int main(int argc, char **argv)
 {
@@ -61,26 +70,15 @@ int main(int argc, char **argv)
     ble_client = new BleClient(adapter.c_str(), device.c_str(), ADDR_TYPE);
     std::replace(device.begin(), device.end(), ':', '_');
 
-    ros::Subscriber sub8 = n.subscribe("/ble_client_node/" + device + "/gattWriteCmd/UInt8Array",
-            1, gattWriteCmdUInt8Array_cb);
-    ros::Subscriber sub16 = n.subscribe("/ble_client_node/" + device + "/gattWriteCmd/UInt16Array",
-            1, gattWriteCmdUInt16Array_cb);
+
+    ros::Subscriber joyDaten = n.subscribe<sensor_msgs::Joy>("/joy", 10, joyCallback);
 
     if(!ble_client->connect())
     {
         ROS_ERROR("Failed to connect to BLE device.");
     }
 
-    bool listenToNotifications;
-    if(n.getParam("listenToNotifications", listenToNotifications)
-            && listenToNotifications == true)
-    {
-        //uint16_t handle = strtoul(listenToHandle.c_str(), NULL, 0);
-        handle_pub = n.advertise<ble_client_msgs::HandleUInt8Array>(
-                "/ble_client_node/" + device + "/notifications", 1000);
-        ble_client->gattListenToNotification(gattNotificationListener_cb);
-    }
-
+    ros::Timer timer1 = n.createTimer(ros::Duration(0.011), sendData);
     ros::spin();
 
     ble_client->disconnect();
